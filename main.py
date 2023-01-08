f73809f5b7396208c743d0cef5d8ecc221b43186
Michael Lutz <michaeljeffreylutz@gmail.com>
2023-01-07 19:55:39 -0800

    changed main.py

--- main.py
+++ main.py
@@ -1,12 +1,78 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+import json
 import os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+import sys
+sys.path.insert(0, '/src/')
+from src.ChatAssist import ChatAssist
 
 app = Flask(__name__)
 
+MONGODB_URL = os.getenv("MONGODB_URL")
+client = MongoClient(MONGODB_URL)
+database = client['chatassist-development']
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# accept post requests
+@app.route("/hello___", methods=["POST"])
+def hello_world():
+    content = request.get_json()
+    res = content['input']
+    d = {'message': res}
+    res_body = json.dumps(d)
+    return res_body, 200
+
+@app.route('/initialize_convo', methods=['POST'])
+def initialize_convo_handler():
+    """
+    A post method that handles a request wih the parameter "input" and writes the value of input
+    """
+    # Getting access to database defined above
+    global database
+    db = database
+
+    content = request.json
+    text = content['question']
+    domain = content['domain']
+    id = str(ObjectId())
+
+    bot = ChatAssist(db=db, id=id, domain=domain, convo_buffer=[])
+    res = bot.converse(text)
+
+    d = {'message': res, 'id': id}
+    res_body = json.dumps(d)
+    # stringify the json
+    return res_body, 200
+
+@app.route('/generate_response', methods=['POST'])
+def generate_response_handler():
+    """
+    A post method that handles a request wih the parameter "input" and writes the value of input
+    """
+    # Getting access to database defined above
+    global database
+    db = database
+
+    content = request.json
+    text = content['question']
+    id = content['conversation_id']
+
+
+    instance = db.ConversationLog.find_one({"_id": ObjectId(id)})
+    log, domain = instance['conversation'], instance['domain']
+    bot = ChatAssist(db=db, id=id, domain=domain, convo_buffer=log) # In the future, have the bot be created by selection / default and stored somewhere
+
+    # Creating the ChatAssist instance after fetching chat log from database
+    
+
+    res = bot.converse(text)
+
+    d = {'message': res, 'id': id}
+    res_body = json.dumps(d)
+    # stringify the json
+    return res_body, 200
+
 
 
 if __name__ == '__main__':
